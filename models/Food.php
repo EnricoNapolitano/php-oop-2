873746eb72b7e979939836ff09822b9524ae74cf
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-20 17:00:40 +0100

    class Food

---
+++ models/Food.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/AnimalCategory.php';
+require_once __DIR__ . '/Products.php';
+
+class Food extends Products
+{
+    public $expiring_date;
+    public $main_ingredient;
+
+    public function __construct($_price, $_name, $_description, $_genre, $_expiring_date, $_main_ingredient, $_url_image = 'https://svg.template.creately.com/MIQyt1kde0t')
+    {
+        parent::__construct($_price, $_name, $_description, $_genre,$_url_image = 'https://svg.template.creately.com/MIQyt1kde0t');
+        $this->expiring_date=$_expiring_date;
+        $this->main_ingredient=$_main_ingredient;
+    }
+}
+?>
