6af29bb321202d0eada5cc7399b9a5154afd43e8
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-20 18:45:02 +0100

    change of construct in class Food

--- models/Food.php
+++ models/Food.php
@@ -8,9 +8,9 @@ class Food extends Products
     public $expiring_date;
     public $main_ingredient;
 
-    public function __construct($_price, $_name, $_description, $_genre, $_expiring_date, $_main_ingredient, $_url_image = 'https://svg.template.creately.com/MIQyt1kde0t')
+    public function __construct($_price, $_name, $_description, $_animal_category, $_expiring_date, $_main_ingredient, $_url_image = 'https://svg.template.creately.com/MIQyt1kde0t')
     {
-        parent::__construct($_price, $_name, $_description, $_genre,$_url_image = 'https://svg.template.creately.com/MIQyt1kde0t');
+        parent::__construct($_price, $_name, $_description, $_animal_category, $_url_image = 'https://svg.template.creately.com/MIQyt1kde0t');
         $this->expiring_date=$_expiring_date;
         $this->main_ingredient=$_main_ingredient;
     }
