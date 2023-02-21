b9dc45d468811814399aefbaf81c9ca80afd82c1
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-21 22:06:22 +0100

    class Cage, private keys, getters and setters

--- models/Cage.php
+++ models/Cage.php
@@ -3,16 +3,46 @@ require_once __DIR__ . '/Product.php';
 
 class Cage extends Product
 {
-    public $width;
-    public $height;
-    public $depth;
+    private $width;
+    private $height;
+    private $depth;
 
-    public function __construct($_price, $_name, $_description, $_animal_category, $_width, $_height, $_depth, $_url_image = 'https://svg.template.creately.com/MIQyt1kde0t')
+    public function __construct($price, $name, $description, $category, $width, $height, $depth, $url_image = 'https://svg.template.creately.com/MIQyt1kde0t')
     {
-        parent::__construct($_price, $_name, $_description, $_animal_category, $_url_image);
-        $this->width=$_width;
-        $this->height=$_height;
-        $this->depth=$_depth;
+        parent::__construct($price, $name, $description, $category, $url_image);
+        $this->setWidth($width);
+        $this->setHeight($height);
+        $this->setDepth($depth);
+    }
+
+    public function setWidth($width)
+    {
+        $this->width=$width;
+    }
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function setHeight($height)
+    {
+        $this->height=$height;
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function setDepth($depth)
+    {
+        $this->depth=$depth;
+    }
+
+    public function getDepth()
+    {
+        return $this->depth;
     }
 }
 ?>
